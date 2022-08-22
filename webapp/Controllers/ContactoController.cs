8380885889b7f0092de4451af32f3e3ec269cb6d
Margaret07OT <margaret_olivos@usmp.pe>
2022-08-22 18:00:11 -0500

    primer cambio

---
+++ webapp/Controllers/ContactoController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using webapp.Models;
+
+namespace webapp.Controllers
+{
+    
+    public class ContactoController : Controller
+    {
+        private readonly ILogger<ContactoController> _logger;
+
+        public ContactoController(ILogger<ContactoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+         public IActionResult create(Contacto objContacto)
+        {
+
+            ViewData["Message"] = "Se registró satifactoriamente";
+            return View("Index");
+            
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
